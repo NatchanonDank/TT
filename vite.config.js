a5b00aa22b8227fb6cbcd0cd56c481ddfd7dffd5
wlq <165661345+Pphaka@users.noreply.github.com>
2025-11-20 22:51:49 +0700

    สาธุ

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,  // ⭐ ต้องมี ไม่งั้น external host เข้าไม่ได้เลย
-    allowedHosts: [
-      "https://070d79d79ff2.ngrok-free.app"  // ⭐ ต้องใส่โดเมนจริงตรงๆ
-    ]
-  }
 });
